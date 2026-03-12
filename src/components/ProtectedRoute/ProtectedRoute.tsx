dc03ad4caf4a50c19444fcca68c6a63e185d5fbb
Victor <vitorio.hernandez@alumnos.cei.es>
2026-03-12 19:09:24 +0100

    Refactor ProtectedRoute component for improved role-based access control and user handling

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,18 +1,31 @@
 import { Navigate } from "react-router-dom"
 
+// Definimos como es nuestro usuario
+interface User {
+    id: number;
+    name: string;
+    role: 'admin' | 'user'; //Tipado estricto para evitar errores de tipeo
+}
+
 interface Props {
-  isAllowed: boolean;
-  userRole?: string;
-  requiredRole?: string;
-  children: React.ReactNode; 
+    user: User | null; //El usuario puede ser nulo si no ha iniciado sesión
+    requiredRole?: 'admin' | 'user';
+    children: React.ReactNode; //Los componentes hijos que se renderizarán si el usuario tiene acceso 
 }
 
-const ProtectedRoute = ({ isAllowed, userRole, requiredRole, children }: Props) => {
-  if (!isAllowed) return <Navigate to="/login" />
-  if (requiredRole && userRole !== requiredRole) return <Navigate to="/unauthorized" />
-
-  return <div>{children}</div>
+const ProtectedRoute = ({user, requiredRole, children}:Props) => {
+    //Si no esta permitido, lo enviamos al Login inmediatamente
+    if (!user) {
+      // Usamos replacepara que no pueda usar el botón de "Atrás" del navegador y volver a la ruta prohibida
+        return <Navigate to="/login" replace />
+    }
+    if (requiredRole && user.role !== requiredRole) {
+      // Si el usuario no tiene el rol requerido, lo redirigimos a una página de "Acceso Denegado" o al inicio
+        return <Navigate to="/denegado" replace />
+    }
+  return (
+    <div>{children}</div>
+  )
 }
-
 
 export default ProtectedRoute
