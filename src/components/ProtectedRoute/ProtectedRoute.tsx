90873b95be099eee7a45fdbbd6594b034537012b
vicherul <vitorio.hernandez@alumnos.cei.es>
2026-03-12 16:01:50 +0100

    Enhance ProtectedRoute with explanatory comment
    Added comment to explain redirection behavior in ProtectedRoute component.

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -8,11 +8,11 @@ interface Props {
 const ProtectedRoute = ({isAllowed, children}:Props) => {
   //Si no esta permitido, lo enviamos al Login inmediatamente
   if(!isAllowed){
-      return <Navigate to="/login"/>
+      return <Navigate to="/login"/> //Si la condición es falsa, el componente no renderiza nada del contenido protegido. En su lugar, utiliza `<Navigate />` de *react-router-dom* para forzar un cambio de URL hacia el login. Es una **redirección declarativa**.
   }
   return (
     <div>{children}</div>
   )
 }
-
+//Si isAllowed es verdadero, el código ignora el if y procede a mostrar los children. El div actúa como un contenedor para el contenido protegido.
 export default ProtectedRoute
